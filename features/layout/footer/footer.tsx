556b1ae9af017a91b26cd3b94bdefa888de6d970
Tyler Hudson <tylerhudson96@gmail.com>
2023-12-05 14:35:33 -0500

    Implemented footer component with responsive CSS per Figma design specs

---
+++ features/layout/footer/footer.tsx
@@ -0,0 +1,35 @@
+import styles from "./footer.module.scss";
+import packageJSON from "../../../package.json";
+import Link from "next/link";
+
+export function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.container}>
+        <div className={styles.contentContainer}>
+          <ul className={styles.directory}>
+            <Link className={styles.directoryLink} href="#">
+              Docs
+            </Link>
+            <Link className={styles.directoryLink} href="#">
+              API
+            </Link>
+            <Link className={styles.directoryLink} href="#">
+              Help
+            </Link>
+            <Link className={styles.directoryLink} href="#">
+              Community
+            </Link>
+          </ul>
+          <div className={styles.logoContainer}>
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img src="/icons/logo-small.svg" alt="logo" />
+          </div>
+          <div className={styles.versionContainer}>
+            <p className={styles.version}>Version: {packageJSON.version}</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
