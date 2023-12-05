c5f0a19da896f8588d40f26024be5de2e8beb7cb
Tyler Hudson <107374040+Hudson-TD@users.noreply.github.com>
2023-12-04 20:41:41 -0500

    ui-kit-button (#10)
    * Implemented reusable button component that has a variety of styles based on state and passed props
    
    * Added storybook entry for the button component validating styling based on state and props
    
    * Implementation of sizing classes for button component and final polish of code & storybook entry

---
+++ features/ui/button/button.stories.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Meta, StoryFn } from "@storybook/react";
+import { Button, ButtonSize, ButtonColor } from "./button";
+
+export default {
+  title: "UI/Button",
+  component: Button,
+  parameters: {
+    // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
+    layout: "centered",
+  },
+} as Meta<typeof Button>;
+
+const Template: StoryFn<typeof Button> = ({
+  size,
+  color,
+  children,
+  isDisabled,
+  onClick,
+}) => (
+  <div style={{ padding: 10 }}>
+    <Button color={color} size={size} isDisabled={isDisabled} onClick={onClick}>
+      {children}
+    </Button>
+  </div>
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  size: ButtonSize.sm,
+  color: ButtonColor.primary,
+  children: "Label",
+};
+Default.parameters = {
+  viewMode: "docs",
+};
