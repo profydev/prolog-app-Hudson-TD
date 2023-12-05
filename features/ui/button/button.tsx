dd957d02bb7cdf57f98e7ecbd90ad4e4e49c843a
Tyler Hudson <tylerhudson96@gmail.com>
2023-12-04 19:32:22 -0500

    Implemented reusable button component that has a variety of styles based on state and passed props

--- features/ui/button/button.tsx
+++ features/ui/button/button.tsx
@@ -1,9 +1,52 @@
-import { ButtonHTMLAttributes } from "react";
 import classNames from "classnames";
 import styles from "./button.module.scss";
 
-export function Button(props: ButtonHTMLAttributes<HTMLButtonElement>) {
+export enum ButtonSize {
+  sm = "sm",
+  md = "md",
+  lg = "lg",
+  xl = "xl",
+}
+
+export enum ButtonColor {
+  default = "default",
+  primary = "primary",
+  secondary = "secondary",
+  gray = "gray",
+  empty = "empty",
+  emptyGray = "emptyGray",
+  error = "error",
+}
+
+type ButtonProps = {
+  children: React.ReactNode;
+  size?: ButtonSize;
+  color?: ButtonColor;
+  className?: string;
+  onClick: () => void;
+  isDisabled?: boolean;
+};
+
+export function Button({
+  children,
+  size = ButtonSize.md,
+  color = ButtonColor.default,
+  className,
+  onClick,
+  isDisabled = false,
+}: ButtonProps) {
   return (
-    <button {...props} className={classNames(styles.button, props.className)} />
+    <button
+      className={classNames(
+        styles.button,
+        styles[size],
+        styles[color],
+        className,
+      )}
+      onClick={onClick}
+      disabled={isDisabled}
+    >
+      {children}
+    </button>
   );
 }
