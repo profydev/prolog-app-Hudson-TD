409b420654f4eb6e09a98013b622bec3b17d0785
Tyler Hudson <tylerhudson96@gmail.com>
2023-12-08 15:17:24 -0500

    Implemented landing page header per Figma design specs with responsive mobile and desktop viewport styling variants

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,88 @@
+import { useState, useEffect } from "react";
 import { Routes } from "@config/routes";
+import { Button } from "../features/ui/button/button";
+import Link from "next/link";
 import styles from "./index.module.scss";
+import classNames from "classnames";
 
-const IssuesPage = () => {
+const LandingPage = () => {
+  const [viewportWidth, setViewportWidth] = useState(0);
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const [isMobileViewport, setMobileViewport] = useState(false);
+
+  useEffect(() => {
+    const handleViewportUpdate = () => {
+      const newViewport = window.innerWidth;
+      setViewportWidth(newViewport);
+      viewportWidth < 1024 ? setMobileViewport(true) : setMobileViewport(false);
+    };
+    // Set initial viewport width
+    handleViewportUpdate();
+    window.addEventListener("resize", handleViewportUpdate);
+    // Cleanup eventlistener on unmount
+    return () => window.removeEventListener("resize", handleViewportUpdate);
+  });
   return (
     <div>
       <header className={styles.header}>
-        {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img src="/icons/logo-large.svg" alt="Prolog logo" />
-        <a href={Routes.projects}>Dashboard</a>
+        <div className={styles.container}>
+          <div className={styles.logoContainer}>
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img src="/icons/logo-large.svg" alt="Prolog logo" />
+          </div>
+          <div
+            className={classNames(
+              styles.navContainer,
+              isMobileMenuOpen && styles.show,
+            )}
+          >
+            <Link className={styles.navLink} href={Routes.home}>
+              Home
+            </Link>
+            <Link className={styles.navLink} href={Routes.products}>
+              Products
+            </Link>
+            <Link className={styles.navLink} href={Routes.documentation}>
+              Documentation
+            </Link>
+            <Link className={styles.navLink} href={Routes.pricing}>
+              Pricing
+            </Link>
+          </div>
+          <div className={styles.buttonContainer}>
+            <>
+              <Button
+                size="lg"
+                color="primary"
+                className={classNames(
+                  styles.dashboardButton,
+                  isMobileViewport && styles.remove,
+                )}
+                onClick={() => (location.href = Routes.projects)}
+              >
+                Open Dashboard
+              </Button>
+              <Button
+                className={classNames(
+                  styles.dashboardButton,
+                  !isMobileViewport && styles.remove,
+                )}
+                onClick={() => setMobileMenuOpen(!isMobileMenuOpen)}
+              >
+                {/* eslint-disable-next-line @next/next/no-img-element */}
+                <img
+                  src={
+                    isMobileMenuOpen
+                      ? "/icons/landing-close.svg"
+                      : "/icons/landing-menu.svg"
+                  }
+                  alt={isMobileMenuOpen ? "close menu" : "open menu"}
+                  className={styles.menuIcon}
+                />
+              </Button>
+            </>
+          </div>
+        </div>
       </header>
       <button
         className={styles.contactButton}
@@ -24,4 +99,4 @@ const IssuesPage = () => {
   );
 };
 
-export default IssuesPage;
+export default LandingPage;
